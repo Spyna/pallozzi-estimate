75db592480f68e217bb685549821c482f3d00e5a
Lorenzo spinelli <iam.lorenzo.spinelli@gmail.com>
2022-11-01 20:07:25 +0100

    build: copy lib64

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 curl https://github.com/jwerre/node-canvas-lambda/releases/download/v2.0.0/node16_canvas_lib64_layer_x86_64.zip -O -L 
 
 unzip -j -d lib64 node16_canvas_lib64_layer_x86_64.zip 
+unzip -j -d /lib64 node16_canvas_lib64_layer_x86_64.zip 
 
 cp lib64/* node_modules/canvas/build/Release/ 
 
