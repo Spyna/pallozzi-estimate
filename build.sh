73825274fc12f349be7e92d6139b14367518e73f
Lorenzo spinelli <iam.lorenzo.spinelli@gmail.com>
2022-11-01 20:44:32 +0100

    build: essentials operation

--- build.sh
+++ build.sh
@@ -1,27 +1,9 @@
-curl https://github.com/jwerre/node-canvas-lambda/releases/download/v2.0.0/node16_canvas_lib64_layer_x86_64.zip -O -L 
-
-unzip -j -d lib64 node16_canvas_lib64_layer_x86_64.zip 
-
-
-cp lib64/* node_modules/canvas/build/Release/ 
-
-
 yum install libuuid-devel libmount-devel zlib 
 yum reinstall zlib
-
-ls /lib64/
 
 LD_LIBRARY_PATH=/var/task/node_modules/canvas/build/Release
 
 cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/
-
-
-ls node_modules/canvas/build/Release/ 
-
-echo $PATH
-
-echo $LD_LIBRARY_PATH
-echo $LD_PRELOAD
 
 yarn next build
 
