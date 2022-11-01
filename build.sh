d62a819dd291151797b06fefcc15e373fcf1db6c
Lorenzo spinelli <iam.lorenzo.spinelli@gmail.com>
2022-11-01 20:26:28 +0100

    build: reinstall zlib

--- build.sh
+++ build.sh
@@ -5,6 +5,17 @@ unzip -j -d /lib64 node16_canvas_lib64_layer_x86_64.zip
 
 cp lib64/* node_modules/canvas/build/Release/ 
 
+
+yum install libuuid-devel libmount-devel zlib 
+yum reinstall zlib
+
+cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/
+
+
 ls node_modules/canvas/build/Release/ 
 
+echo $LD_LIBRARY_PATH
+echo $LD_PRELOAD
+
 yarn next build
+
